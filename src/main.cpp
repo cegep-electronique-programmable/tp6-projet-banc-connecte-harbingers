97a3e640307d551342c6a5b260875d34e22763ca
Sonia <soso.michaud@icloud.com>
2025-05-06 10:32:00 -0400

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
-
+//allo
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
