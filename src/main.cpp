3dd87bc3e89f85e51b1377bb494c8b11302cc321
Sonia <soso.michaud@icloud.com>
2025-05-06 11:58:23 -0400

    code affichage test

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  startEcran();
 }
 //allo
 // the loop function runs over and over again forever
