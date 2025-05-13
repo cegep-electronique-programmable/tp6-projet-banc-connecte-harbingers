9ec1b589d1fd7b6d5a50dba45b2b10660940db7c
Sonia <soso.michaud@icloud.com>
2025-05-13 11:50:37 -0400

    Affichage de l'écran au complet, main a modifier

--- src/affichage.h
+++ src/affichage.h
@@ -1,73 +1,28 @@
-/*!
- * @file affichage.h
- *
- * @author À compléter
+/*
+ * Nom de fichier 
+ *      affichage.h
  * 
- * Sonia Michaud
+ * Sommaire
+ *      Fichier .h pour l'affichage de l'écran
+ *      Librarie: U8g2lib
+ * Auteur 
+ *      Sonia
  * 
- * @date À compléter
- * 
- * @brief À compléter
- *
+ * Date de création
+ *      13 mai 2025
+ *  
  */
 
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
 
 #include <Arduino.h>
-#include <Adafruit_SSD1306.h>
+#include <U8g2lib.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
-
-static const unsigned char PROGMEM logo_bmp[] =
-{ 0b00000000, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000001, 0b11000000,
-  0b00000011, 0b11100000,
-  0b11110011, 0b11100000,
-  0b11111110, 0b11111000,
-  0b01111110, 0b11111111,
-  0b00110011, 0b10011111,
-  0b00011111, 0b11111100,
-  0b00001101, 0b01110000,
-  0b00011011, 0b10100000,
-  0b00111111, 0b11100000,
-  0b00111111, 0b11110000,
-  0b01111100, 0b11110000,
-  0b01110000, 0b01110000,
-  0b00000000, 0b00110000 };
-
-void startEcran();
-void testdrawline();
-void testdrawrect(void);
-void testfillrect(void);
-void testdrawcircle(void);
-void testfillcircle(void);
-void testdrawroundrect(void);
-void testfillroundrect(void);
-void testdrawtriangle(void);
-void testfilltriangle(void);
-void testdrawchar(void);
-void testdrawstyles(void);
-void testscrolltext(void);
-void testdrawbitmap(void);
-
-#define XPOS   0 // Indexes into the 'icons' array in function below
-#define YPOS   1
-#define DELTAY 2
-
-void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h);
-
-
-//void afficherInfo(uint16_t nbrPresence, uint16_t nbrCharge);
-
-
-
+//déclaration des fonctions
+void startEcran(void);
+void titres(void);
+void line(void);
+void afficher(int,int);
 
 #endif // AFFICHAGE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+/*
+ * Nom de fichier 
+ *      main.cpp
+ * 
+ * Sommaire
+ *      Fichier du projet du banc connecter
+ * 
+ * Auteur 
+ *      Jeff, Sonia, Cesar, Mark
+ * 
+ * Date de création
+ *      13 mai 2025
+ *  
+ */
 #include <Arduino.h>
 #include "affichage.h"
 
@@ -7,11 +21,19 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   startEcran();
 }
-//allo
+
 // the loop function runs over and over again forever
 void loop() {
+  //À enlever? (del qui flash)
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for 1second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(100);                      // wait for 100ms
+  /*
+  Fonction afficher pour l'écran
+  premier nombre = variable en int du nombre de personne passé (so from Cesar)
+  deuxieme nombre = variable en int du nombre de telephone charge (so from Jeff)
+  */
+  afficher(0,1);
+  delay(100);
 }
